cfbc9741e220052dae1d8dad562d93e43f67c7f4
Paulo Rijnberg <paulo.rijnberg@quantile.nl>
2024-01-22 17:32:47 +0100

    Added first version of Tixly tap.

--- tap_tixly/client.py
+++ tap_tixly/client.py
@@ -1,40 +1,27 @@
-"""REST client handling, including TixlyStream base class."""
-
-from __future__ import annotations
-
-import sys
-from typing import Any, Callable, Iterable
-
+from typing import Iterable
 import requests
 from singer_sdk.authenticators import BearerTokenAuthenticator
 from singer_sdk.helpers.jsonpath import extract_jsonpath
-from singer_sdk.pagination import BaseAPIPaginator  # noqa: TCH002
+from singer_sdk.pagination import BaseHATEOASPaginator
 from singer_sdk.streams import RESTStream
+from urllib.parse import parse_qsl
 
-if sys.version_info >= (3, 9):
-    import importlib.resources as importlib_resources
-else:
-    import importlib_resources
 
-_Auth = Callable[[requests.PreparedRequest], requests.PreparedRequest]
-
-# TODO: Delete this is if not using json files for schema definition
-SCHEMAS_DIR = importlib_resources.files(__package__) / "schemas"
+class TixlyPaginator(BaseHATEOASPaginator):
+    """Fetches the next url from the response."""
+    def get_next_url(self, response):
+        data = response.json()
+        return data.get("Next")
 
 
 class TixlyStream(RESTStream):
     """Tixly stream class."""
+    records_jsonpath = "$.Data[*]"
 
     @property
     def url_base(self) -> str:
         """Return the API URL root, configurable via tap settings."""
-        # TODO: hardcode a value here, or retrieve it from self.config
-        return "https://api.mysample.com"
-
-    records_jsonpath = "$[*]"  # Or override `parse_response`.
-
-    # Set this value or override `get_new_paginator`.
-    next_page_token_jsonpath = "$.next_page"  # noqa: S105
+        return self.config["url_base"]
 
     @property
     def authenticator(self) -> BearerTokenAuthenticator:
@@ -45,104 +32,59 @@ class TixlyStream(RESTStream):
         """
         return BearerTokenAuthenticator.create_for_stream(
             self,
-            token=self.config.get("auth_token", ""),
+            token=self.config.get("auth_token"),
         )
 
     @property
     def http_headers(self) -> dict:
-        """Return the http headers needed.
-
-        Returns:
-            A dictionary of HTTP headers.
-        """
-        headers = {}
-        if "user_agent" in self.config:
-            headers["User-Agent"] = self.config.get("user_agent")
-        # If not using an authenticator, you may also provide inline auth headers:
-        # headers["Private-Token"] = self.config.get("auth_token")  # noqa: ERA001
+        """Return the http headers needed."""
+        headers = {
+            "Content-Type": "application/json",
+        }
         return headers
 
-    def get_new_paginator(self) -> BaseAPIPaginator:
-        """Create a new pagination helper instance.
+    def get_new_paginator(self) -> TixlyPaginator:
+        return TixlyPaginator()
 
-        If the source API can make use of the `next_page_token_jsonpath`
-        attribute, or it contains a `X-Next-Page` header in the response
-        then you can remove this method.
+    def get_url_params(self, context, next_page_token):
+        params = {
+            "pageSize": 500,
+        }
 
-        If you need custom pagination that uses page numbers, "next" links, or
-        other approaches, please read the guide: https://sdk.meltano.com/en/v0.25.0/guides/pagination-classes.html.
-
-        Returns:
-            A pagination helper instance.
-        """
-        return super().get_new_paginator()
-
-    def get_url_params(
-        self,
-        context: dict | None,  # noqa: ARG002
-        next_page_token: Any | None,  # noqa: ANN401
-    ) -> dict[str, Any]:
-        """Return a dictionary of values to be used in URL parameterization.
-
-        Args:
-            context: The stream context.
-            next_page_token: The next page index or value.
-
-        Returns:
-            A dictionary of URL query parameters.
-        """
-        params: dict = {}
+        # Next page token is a URL, so we can to parse it to extract the query string
         if next_page_token:
-            params["page"] = next_page_token
+            params.update(parse_qsl(next_page_token.query))
+
         if self.replication_key:
-            params["sort"] = "asc"
-            params["order_by"] = self.replication_key
+            start_time = self.get_starting_timestamp(context)
+            start_time_fmt = start_time.strftime("%Y-%m-%dT%H:%M:%SZ") if start_time else None
+            params["DateFrom"] = start_time_fmt
+
         return params
 
-    def prepare_request_payload(
-        self,
-        context: dict | None,  # noqa: ARG002
-        next_page_token: Any | None,  # noqa: ARG002, ANN401
-    ) -> dict | None:
-        """Prepare the data payload for the REST API request.
-
-        By default, no payload will be sent (return None).
-
-        Args:
-            context: The stream context.
-            next_page_token: The next page index or value.
-
-        Returns:
-            A dictionary with the JSON body for a POST requests.
-        """
-        # TODO: Delete this method if no payload is required. (Most REST APIs.)
-        return None
-
     def parse_response(self, response: requests.Response) -> Iterable[dict]:
-        """Parse the response and return an iterator of result records.
-
-        Args:
-            response: The HTTP ``requests.Response`` object.
-
-        Yields:
-            Each record from the source.
-        """
-        # TODO: Parse response body and return a set of records.
+        """Parse the response and return an iterator of result records."""
         yield from extract_jsonpath(self.records_jsonpath, input=response.json())
 
     def post_process(
         self,
         row: dict,
-        context: dict | None = None,  # noqa: ARG002
-    ) -> dict | None:
-        """As needed, append or transform raw data to match expected structure.
-
-        Args:
-            row: An individual record from the stream.
-            context: The stream context.
-
-        Returns:
-            The updated record dictionary, or ``None`` to skip the record.
+        context: dict,
+    ) -> dict:
         """
-        # TODO: Delete this method if not needed.
-        return row
+        We introduce this post process method due to finding the correct replication value.
+
+        The desired replication key exists in either the "Edited" or "Created" column. This
+        is because newly added records only contain a "Created" value and not an "Edited"
+        value. The "Edited" column contains a null (parsed to None) for newly created records.
+        """
+        if row["Edited"] == None:
+            _updated_at = row["Created"]
+
+        else:
+            _updated_at = row["Edited"]
+
+        return {
+            **row,
+            "_updated_at": _updated_at,
+        }
--- tap_tixly/tap.py
+++ tap_tixly/tap.py
@@ -1,20 +1,20 @@
 """Tixly tap class."""
 
-from __future__ import annotations
+from singer_sdk import Tap, Stream
+from singer_sdk import typing as th
 
-from singer_sdk import Tap
-from singer_sdk import typing as th  # JSON schema typing helpers
-
-# TODO: Import your custom stream types here:
 from tap_tixly import streams
+from typing import List
+
+STREAM_TYPES = [
+    streams.CustomersStream,
+]
 
 
 class TapTixly(Tap):
     """Tixly tap class."""
-
     name = "tap-tixly"
 
-    # TODO: Update this section with the actual config values you expect:
     config_jsonschema = th.PropertiesList(
         th.Property(
             "auth_token",
@@ -24,34 +24,21 @@ class TapTixly(Tap):
             description="The token to authenticate against the API service",
         ),
         th.Property(
-            "project_ids",
-            th.ArrayType(th.StringType),
-            required=True,
-            description="Project IDs to replicate",
-        ),
-        th.Property(
             "start_date",
             th.DateTimeType,
             description="The earliest record date to sync",
         ),
         th.Property(
-            "api_url",
+            "url_base",
             th.StringType,
-            default="https://api.mysample.com",
-            description="The url for the API service",
+            default="https://crmapi.tixnl.nl/v3/",
+            description="The base url for the API service for Tixly.",
         ),
     ).to_dict()
 
-    def discover_streams(self) -> list[streams.TixlyStream]:
-        """Return a list of discovered streams.
-
-        Returns:
-            A list of discovered streams.
-        """
-        return [
-            streams.GroupsStream(self),
-            streams.UsersStream(self),
-        ]
+    def discover_streams(self) -> List[Stream]:
+        """Return a list of discovered streams."""
+        return [stream_class(tap=self) for stream_class in STREAM_TYPES]
 
 
 if __name__ == "__main__":
